609e9fe5f37077a7bedae7610d1548ce54ec1757
Arthur Philippe <arthur.philippe@epitech.eu>
2018-02-11 10:16:59 +0100

    fixed negative bytes

--- objdump/include/print_byte.h
+++ objdump/include/print_byte.h
@@ -11,17 +11,12 @@
 	#include <stdio.h>
 	#include <ctype.h>
 
-inline static char print_byte(char byte)
+inline static char print_byte(unsigned char byte)
 {
 	if (byte > 0) {
 		printf("%02x", byte);
-		// dprintf(2, "%d -> %02x\n", byte, byte);
 	} else if (byte == 0) {
 		printf("00");
-		// dprintf(2, "%d -> 00\n", byte);
-	} else {
-		// printf("%02x", -byte);
-		printf("--");
 	}
 	return ((!isprint(byte)) ? '.' : byte);
 }
