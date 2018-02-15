fdcde4bc7de5a1875575ae46223b66dde21d89d9
Arthur Philippe <arthur.philippe@epitech.eu>
2018-02-15 09:45:33 +0100

    test that for me

--- nm/src/type_symbol.c
+++ nm/src/type_symbol.c
@@ -35,10 +35,15 @@ char type_functor_btr(elf_t *elf, Elf64_Sym *sym)
 {
 	if (symbol_is_from_section(sym, elf, ".bss"))
 		return ('b');
-	else if (symbol_is_from_section(sym, elf, ".text")
-		|| ELF64_ST_TYPE(sym->st_info) == STT_FUNC)
+	else if ((symbol_is_from_section(sym, elf, ".text")
+			|| ELF64_ST_TYPE(sym->st_info) == STT_FUNC)
+		|| (elf->sh_table[sym->st_shndx].sh_type == SHT_PROGBITS
+			&& elf->sh_table[sym->st_shndx].sh_flags ==
+				(SHF_ALLOC | SHF_EXECINSTR))
+		|| elf->sh_table[sym->st_shndx].sh_type > SHT_DYNSYM)
 		return ('t');
-	else if (symbol_is_from_section(sym, elf, ".rodata"))
+	else if (elf->sh_table[sym->st_shndx].sh_type == SHT_PROGBITS
+			&& elf->sh_table[sym->st_shndx].sh_flags == SHF_ALLOC)
 		return ('r');
 	return (0);
 }
