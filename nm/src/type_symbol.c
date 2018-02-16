7206196949c3fa3f2d4608bea9d7efc3050abd95
Arthur Philippe <arthur.philippe@epitech.eu>
2018-02-16 12:48:09 +0100

    removed old functors

--- nm/src/type_symbol.c
+++ nm/src/type_symbol.c
@@ -22,34 +22,6 @@ char (*const TYPE_FUNCTORS[]) (elf_t *elf, Elf64_Sym *sym) = {
 	NULL,
 };
 
-char type_functor_cun(elf_t *elf, Elf64_Sym *sym)
-{
-	if (sym->st_shndx == SHN_COMMON)
-		return ('C');
-	else if (sym->st_shndx == SHN_UNDEF)
-		return ('U');
-	else if (symbol_is_from_section(sym, elf, ".debug"))
-		return ('N');
-	return (0);
-}
-
-char type_functor_btr(elf_t *elf, Elf64_Sym *sym)
-{
-	if (symbol_is_from_section(sym, elf, ".bss"))
-		return ('b');
-	else if ((symbol_is_from_section(sym, elf, ".text")
-			|| ELF64_ST_TYPE(sym->st_info) == STT_FUNC)
-		|| (elf->sh_table[sym->st_shndx].sh_type == SHT_PROGBITS
-			&& elf->sh_table[sym->st_shndx].sh_flags ==
-				(SHF_ALLOC | SHF_EXECINSTR))
-		|| elf->sh_table[sym->st_shndx].sh_type > SHT_DYNSYM)
-		return ('t');
-	else if (elf->sh_table[sym->st_shndx].sh_type == SHT_PROGBITS
-			&& elf->sh_table[sym->st_shndx].sh_flags == SHF_ALLOC)
-		return ('r');
-	return (0);
-}
-
 char type_symbol(elf_t *elf, Elf64_Sym *sym)
 {
 	char ret;
